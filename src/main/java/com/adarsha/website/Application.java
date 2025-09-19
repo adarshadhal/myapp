f265489f2ce7bc83c81ebe5171cfd76a20245efa
Adarsha <mailme@gmail.com>
2025-09-19 18:52:53 +0530

    Create a new File

--- src/main/java/com/adarsha/website/Application.java
+++ src/main/java/com/adarsha/website/Application.java
@@ -1,18 +1,19 @@
 package com.adarsha.website;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        // SpringApplicationBuilder helps in setting up the application configuration
         return application.sources(Application.class);
     }
 
     public static void main(String[] args) {
+        // This starts the Spring Boot application
         SpringApplication.run(Application.class, args);
     }
 }
