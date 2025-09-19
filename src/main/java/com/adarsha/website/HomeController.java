7120c063d75d5d1144d209e680dc5f7d3fa84490
Adarsha <mailme@gmail.com>
2025-09-19 18:37:41 +0530

    Create my New Web site

---
+++ src/main/java/com/adarsha/website/HomeController.java
@@ -0,0 +1,14 @@
+package com.adarsha.website;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";  // Return the "index.html" template
+    }
+}
+
